95384b6746fd07bca12964759ea3c15e622069d6
Joao Pedro <jpbertelli10@gmail.com>
2023-03-06 15:01:07 -0300

    lista 1

---
+++ NotaAprovadoReprovado.py
@@ -0,0 +1,11 @@
+# Lê os três números inteiros do usuário
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+media = (num1 + num2 + num3) / 3
+if media >= 7:
+    print("Aprovado")
+elif media >= 5:
+    print("Recuperação")
+else:
+    print("Reprovado")
